146149af25dcb274edc222c097acf4ee28e33082
Farhan Hasan <hasanfarhan33@gmail.com>
2021-07-25 14:08:26 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("circle1").onclick = function () {
+  document.getElementById("circle1").style.display = "none";
+};
+
+// Why does this not work?
+// document.getElementsByClassName("circle2").onclick = function () {
+//   document.getElementsByClassName("circle2").style.display = "none";
+// };
+
+document.getElementById("circle2").onclick = function () {
+  document.getElementById("circle2").style.display = "none";
+};
+
+document.getElementById("circle3").onclick = function () {
+  document.getElementById("circle3").style.display = "none";
+};
